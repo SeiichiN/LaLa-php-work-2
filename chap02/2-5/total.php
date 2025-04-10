c7ed6966ebc3ccc05258a85ea59392e1bdf621f8
SeiichiN <billie175@gmail.com>
2025-04-10 16:29:56 +0900

    4/10

---
+++ chap02/2-5/total.php
@@ -0,0 +1,8 @@
+<?php
+$total = 80 + 40;
+$result = $total - 5;
+echo "合計{$total}、最終結果{$result}";
+
+$a = 5;
+$b = $a / 0;
+echo $b;
---
+++ chap03/3-1/if_else_false.php
@@ -0,0 +1,8 @@
+<?php
+$tokuten = 45;
+if ($tokuten >= 80) {
+  echo "素晴らしい！";
+} else {
+  echo "もう少しがんばりましょう！";
+}
+echo "{$tokuten}点でした。";
