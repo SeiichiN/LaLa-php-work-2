20ef1ab1950af2b29d219844a198eb8d50ef0b71
SeiichiN <billie175@gmail.com>
2025-04-24 08:12:42 +0900

    chap09 修正

---
+++ chap09/9-1/input_radio_alt/profile.php
@@ -0,0 +1,88 @@
+<?php
+require_once('../../lib/util2.php');
+
+if (!mb_check_encoding($_POST, 'UTF-8')) {
+  $err = "Encoding Error! The expected encoding is UTF-8";
+  die($err);  
+}
+
+$error = [];
+if (isset($_POST['sex'])) {
+  $sexValues = ['男性', '女性'];
+  $isSex = in_array($_POST['sex'], $sexValues);
+  if ($isSex) {
+    $sex = $_POST['sex'];
+  } else {
+    $sex = 'error';
+    $error[] = "「性別」に入力エラーがありました";
+  }
+} else {
+  $isSex = false;
+  $sex = "男性";
+}  // sex
+
+if (isset($_POST['marriage'])) {
+  $marriageValue = ['独身', '既婚', '同棲中'];
+  $isMarriage = in_array($_POST['marriage'], $marriageValue);
+  if ($isMarriage) {
+    $marriage = $_POST['marriage'];
+  } else {
+    $marriage = 'error';
+    $error[] = '「結婚」に入力エラーがありました';
+  }
+} else {
+  $isMarriage = false;
+  $marriage = "独身";
+}
+// $value -- '男性', '女性'
+// $checkedValues -- ['男性'] or ['女性']
+function checked(string $value, array $checkedValues) {
+  $isCkecked = in_array($value, $checkedValues);
+  if ($isCkecked) {
+    echo 'checked';
+  }
+}
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="../../css/style.css">
+</head>
+
+<body>
+  <div>
+    <form method="post" action="">
+      <ul>
+        <li><span>性別：</span>
+          <label><input type="radio" name="sex" value="男性"
+                  <?php checked('男性', [$sex]) ?>>男性</label>
+          <label><input type="radio" name="sex" value="女性"
+                  <?php checked('女性', [$sex]) ?>>女性</label>
+        </li>
+        <li><span>結婚：</span>
+          <label><input type="radio" name="marriage" value="独身"
+                  <?php checked('独身', [$marriage]) ?>>独身</label>
+          <label><input type="radio" name="marriage" value="既婚"
+                  <?php checked('既婚', [$marriage]) ?>>既婚</label>
+          <label><input type="radio" name="marriage" value="同棲中"
+                  <?php checked('同棲中', [$marriage]) ?>>同棲中</label>
+        </li>
+        <li><input type="submit" value="送信する"></li>
+      </ul>
+    </form>
+    <?php
+    $isSubmited = $isSex && $isMarriage;
+    if ($isSubmited) {
+      echo '<hr>';
+      echo h("あなたは「{$sex}、{$marriage}」です。");
+    }
+    if (count($error) > 0) {
+      printError($error);
+    }
+    ?>
+  </div>
+</body>
+</html>
---
+++ chap09/9-1/input_radio_alt/profile2.php
@@ -0,0 +1,79 @@
+<?php
+require_once('../../lib/util2.php');
+
+if (!mb_check_encoding($_POST, 'UTF-8')) {
+  $err = "Encoding Error! The expected encoding is UTF-8";
+  die($err);  
+}
+
+$error = [];
+if (isset($_POST['sex'])) {
+  $sexValues = ['男性', '女性'];
+  $isSex = in_array($_POST['sex'], $sexValues);
+  if ($isSex) {
+    $sex = $_POST['sex'];
+  } else {
+    $sex = 'error';
+    $error[] = "「性別」に入力エラーがありました";
+  }
+} else {
+  $isSex = false;
+  $sex = "女性";
+}  // sex
+
+if (isset($_POST['marriage'])) {
+  $marriageValue = ['独身', '既婚', '同棲中'];
+  $isMarriage = in_array($_POST['marriage'], $marriageValue);
+  if ($isMarriage) {
+    $marriage = $_POST['marriage'];
+  } else {
+    $marriage = 'error';
+    $error[] = '「結婚」に入力エラーがありました';
+  }
+} else {
+  $isMarriage = false;
+  $marriage = "独身";
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <div>
+    <form method="post" action="">
+      <ul>
+        <li><span>性別：</span>
+          <label><input type="radio" name="sex" value="男性"
+                  <?php echo ($sex == '男性' ? 'checked' : '') ?>>男性</label>
+          <label><input type="radio" name="sex" value="女性"
+                  <?php echo ($sex == '女性' ? 'checked' : '') ?>>女性</label>
+        </li>
+        <li><span>結婚：</span>
+          <label><input type="radio" name="marriage" value="独身"
+                  <?php echo ($marriage == '独身' ? 'checked' : '') ?>>独身</label>
+          <label><input type="radio" name="marriage" value="既婚"
+                  <?php echo ($marriage == '既婚' ? 'checked' : '') ?>>既婚</label>
+          <label><input type="radio" name="marriage" value="同棲中"
+                  <?php echo ($marriage == '同棲中' ? 'checked' : '') ?>>同棲中</label>
+        </li>
+        <li><input type="submit" value="送信する"></li>
+      </ul>
+    </form>
+    <?php
+    $isSubmited = $isSex && $isMarriage;
+    if ($isSubmited) {
+      echo '<hr>';
+      echo h("あなたは「{$sex}、{$marriage}」です。");
+    }
+    if (count($error) > 0) {
+      printError($error);
+    }
+    ?>
+  </div>
+</body>
+</html>
