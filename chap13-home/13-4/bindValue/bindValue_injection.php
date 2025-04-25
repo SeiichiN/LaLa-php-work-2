6760f3a1e24de777d5f4caa042b604f5a4f7159b
SeiichiN <billie175@gmail.com>
2025-04-25 16:27:55 +0900

    chap13-home

--- chap13-home/13-4/bindValue/bindValue_injection.php
+++ chap13-home/13-4/bindValue/bindValue_injection.php
@@ -6,6 +6,13 @@ $dbName = 'testdb';
 $host = 'localhost:3306';
 $dsn = "mysql:host={$host};dbname={$dbName};charset=utf8";
 ?>
+<?php
+if (isset($_POST['name'])) {
+  $name = $_POST['name'];
+} else {
+  $name = '';
+}
+?>
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -23,8 +30,7 @@ $dsn = "mysql:host={$host};dbname={$dbName};charset=utf8";
       $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
       $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       echo "データベース{$dbName}に接続しました。";
-      $sql = "SELECT * FROM member " . 
-             " WHERE min >= {$min} AND max <= {$max} AND sex = '{$sex}'";
+      $sql = "SELECT * FROM member WHERE name like '%{$name}%'";
       $stm = $pdo->prepare($sql);
       $stm->execute();
       $result = $stm->fetchAll(PDO::FETCH_ASSOC);
@@ -33,7 +39,6 @@ $dsn = "mysql:host={$host};dbname={$dbName};charset=utf8";
       echo "<th>", "ID", "</th>";
       echo "<th>", "名前", "</th>";
       echo "<th>", "年齢", "</th>";
-      echo "<th>", "性別", "</th>";
       echo "</tr></thead>";
       echo "<tbody>";
       foreach ($result as $row) {
@@ -41,7 +46,6 @@ $dsn = "mysql:host={$host};dbname={$dbName};charset=utf8";
         echo "<td>", h($row['id']), "</td>";
         echo "<td>", h($row['name']), "</td>";
         echo "<td>", h($row['age']), "</td>";
-        echo "<td>", h($row['sex']), "</td>";
         echo "</tr>";
       }
       echo "</tbody>";
