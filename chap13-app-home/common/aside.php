4457a16c97b9863b6440f24bde1b745396065567
SeiichiN <billie175@gmail.com>
2025-05-01 07:44:07 +0900

    chap13-app-home/新規登録

--- chap13-app-home/common/aside.php
+++ chap13-app-home/common/aside.php
@@ -4,4 +4,7 @@
     <input type="text" name="name" placeholder="名前">
     <input type="submit" value="検索">
   </form>
+  <form action="insertForm.php" methos="post">
+    <input type="submit" value="新規登録">
+  </form>
 </aside>
--- chap13-app-home/common/printTable.php
+++ chap13-app-home/common/printTable.php
@@ -1,26 +1,25 @@
 <?php
 echo "<table>";
-  echo "<thead>
+echo "<thead>
     <tr>";
-      echo "<th>", "ID", "</th>";
-      echo "<th>", "名前", "</th>";
-      echo "<th>", "年齢", "</th>";
-      echo "<th>", "性別", "</th>";
-      echo "<th></th>";
-      echo "<th></th>";
-      echo "</tr>
+echo "<th>", "ID", "</th>";
+echo "<th>", "名前", "</th>";
+echo "<th>", "年齢", "</th>";
+echo "<th>", "性別", "</th>";
+echo "<th></th>";
+echo "<th></th>";
+echo "</tr>
   </thead>";
-  echo "<tbody>";
-    foreach ($result as $row) {
-    echo "<tr>";
-      echo "<td>", h($row['id']), "</td>";
-      echo "<td>", h($row['name']), "</td>";
-      echo "<td>", h($row['age']), "</td>";
-      echo "<td>", h($row['sex']), "</td>";
-      echo "<td><a href=\"update.php?id={$row['id']}\">編集</a></td>";
-      echo "<td><a href=\"delete.php?id={$row['id']}\">削除</a></td>";
-      echo "</tr>";
-    }
-    echo "</tbody>";
-  echo "</table>";
-  
+echo "<tbody>";
+foreach ($result as $row) {
+  echo "<tr>";
+  echo "<td>", h($row['id']), "</td>";
+  echo "<td>", h($row['name']), "</td>";
+  echo "<td>", h($row['age']), "</td>";
+  echo "<td>", h($row['sex']), "</td>";
+  echo "<td><a href=\"update.php?id={$row['id']}\">編集</a></td>";
+  echo "<td><a href=\"delete.php?id={$row['id']}\">削除</a></td>";
+  echo "</tr>";
+}
+echo "</tbody>";
+echo "</table>";
